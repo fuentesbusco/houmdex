653bf4ecd3b54d95c0d75a1fcaef2de4f76caa99
fuentesbusco <63988039+fuentesbusco@users.noreply.github.com>
2021-01-04 09:53:11 -0300

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,50 +1,8 @@
 import React, { useEffect } from 'react';
-//import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 const Error = props => {
-    //const [data, setData] = useState(null);
-    //const [error, setError] = useState(null);
-
-    useEffect(() => {
-        /*const CancelToken = axios.CancelToken;
-        const source = CancelToken.source();
-        const getMachineCode = async () => {
-            try {
-                let result = await axios.get(`sensors/machine_code.txt`, { cancelToken: source.token });
-                if (result.data) {
-                    result.data = result.data.replace(/(\r\n|\n|\r)/gm,"");
-                }
-                localStorage.setItem('machineCode', result.data);
-            } catch (error) {
-                if (axios.isCancel(error)) {
-                    console.log("axios cancelled");
-                } else {
-                        if (error.response) {
-                            setError("Ha ocurrido un error en el servidor");
-                        } else if (error.request) {
-                            setError("Verifique la conexión a internet");
-                        } else {
-                            setError("Error al cargar datos");
-                        }
-                    }
-            }
-        }
-
-        getMachineCode();
-
-        return () => {
-            source.cancel();
-        }
-    */
-    },[]);
-
-    useEffect(() => {
-        //const CancelToken = axios.CancelToken;
-        //const source = CancelToken.source();
-    }, []);
-
     return (
         <Grid
         container
